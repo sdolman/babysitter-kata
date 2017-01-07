7a28654d96ea134bdfcfc7543afd8ed8e22b1912
Seth Dolman <sdolman@users.noreply.github.com>
2017-01-07 15:59:48 -0500

    start fractional hour paid in full

--- src/babysitter/BabysitterKata.java
+++ src/babysitter/BabysitterKata.java
@@ -7,6 +7,9 @@ public class BabysitterKata {
 	private int endTime;
 
 	public BabysitterKata(int startTime, int endTime) {
+		if(startTime % 100 != 0) {
+			startTime = startTime - (startTime % 100);
+		}
 		this.startTime = startTime;
 		if(endTime % 100 != 0) {
 			endTime = endTime + 100 - (endTime % 100);
