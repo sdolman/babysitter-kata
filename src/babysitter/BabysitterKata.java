ccca2d4cd8a3b9bc02749ba3d855b1b28834c0da
Seth Dolman <sdolman@users.noreply.github.com>
2017-01-07 19:10:28 -0500

    refactoring

--- src/babysitter/BabysitterKata.java
+++ src/babysitter/BabysitterKata.java
@@ -1,37 +1,47 @@
 package babysitter;
 
 public class BabysitterKata {
-	
+
+	private static final int BEDTIME_RATE = 8;
+	private static final int EVENING_RATE = 12;
 	private int amountDue;
 	private int startTime;
 	private int endTime;
 	private int bedtime;
 
 	public BabysitterKata(int startTime, int endTime, int bedtime) {
-		if(startTime % 100 != 0) {
-			startTime = startTime - (startTime % 100);
-		}
-		this.startTime = startTime;
-		if(endTime % 100 != 0) {
-			endTime = endTime + 100 - (endTime % 100);
-		}
-		this.endTime = endTime;
+		this.startTime = topOfTheHour(startTime);
+		this.endTime = extendTimeToNextWholeHour(endTime);
 		this.bedtime = bedtime;
 	}
 
-	public int getAmountDue() {
-		if (endTime <= bedtime) {
-		amountDue += (endTime - startTime)/100*12;
+	private int topOfTheHour(int startTime) {
+		return startTime - (startTime % 100);
+	}
+
+	private int extendTimeToNextWholeHour(int endTime) {
+		if (endTime % 100 != 0) {
+			endTime = endTime + 100 - (endTime % 100);
 		}
-		if (endTime > bedtime && startTime >= bedtime) {
-			amountDue = (endTime - startTime)/100*8;
-		}
-		if (endTime > bedtime && startTime < bedtime) {
-			amountDue += (bedtime - startTime)/100*12;
-			amountDue += (endTime - bedtime)/100*8;
-		}
-		
+		return endTime;
+	}
+
+	public int calculateAmountDue() {
+		addAmountDuePriorToBedtime();
+		addAmountDueAfterBedtime();
 		return amountDue;
+	}
+
+	private void addAmountDuePriorToBedtime() {
+		if (startTime < bedtime) {
+			amountDue += (Math.min(endTime, bedtime) - startTime) / 100 * EVENING_RATE;
+		}
+	}
+
+	private void addAmountDueAfterBedtime() {
+		if (endTime > bedtime) {
+			amountDue += (endTime - Math.max(startTime, bedtime)) / 100 * BEDTIME_RATE;
+		}
 	}
 
 }
--- test/babysitter/BabysitterKataTest.java
+++ test/babysitter/BabysitterKataTest.java
@@ -11,37 +11,37 @@ public class BabysitterKataTest {
 	@Test
 	public void whenWorking5PmTo6PmThenPay12() {
 		underTest = new BabysitterKata(1700, 1800, 2000);
-		assertEquals(12, underTest.getAmountDue());
+		assertEquals(12, underTest.calculateAmountDue());
 	}
 	
 	@Test
 	public void whenWorking2HoursBeforeBedtimeThenPay24 () {
 		underTest = new BabysitterKata(1700, 1900, 2000);
-		assertEquals(24, underTest.getAmountDue());
+		assertEquals(24, underTest.calculateAmountDue());
 	}
 	
 	@Test
 	public void whenWorking2AndFractionHoursPostEndThenPay36 () {
 		underTest = new BabysitterKata(1700, 1915, 2000);
-		assertEquals(36, underTest.getAmountDue());
+		assertEquals(36, underTest.calculateAmountDue());
 	}
 	
 	@Test
 	public void whenWorking1AndFractionHoursPostStartAndPostEndThenPay36 () {
 		underTest = new BabysitterKata(1720, 1915, 2000);
-		assertEquals(36, underTest.getAmountDue());
+		assertEquals(36, underTest.calculateAmountDue());
 	}
 	
 	@Test
 	public void whenWorking1HourAfterBedtimeThenPay8() {
 		underTest = new BabysitterKata(2000, 2100, 2000);
-		assertEquals(8, underTest.getAmountDue());
+		assertEquals(8, underTest.calculateAmountDue());
 	}
 	
 	@Test
 	public void whenWorking1HourBeforeBedtimeAnd2HoursAfterThenPay28() {
 		underTest = new BabysitterKata(1900, 2200, 2000);
-		assertEquals(28, underTest.getAmountDue());
+		assertEquals(28, underTest.calculateAmountDue());
 	}
 
 }
